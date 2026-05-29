e50f369467781dfce3aa4003303292689a0ce1e9
rygyfygy <237001230+rygyfygy@users.noreply.github.com>
2026-05-29 14:18:24 +0200

    Make the layer opaque instead of hidden on lack of item, so it won't break the nav layout.

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -8,26 +8,35 @@ export default function NavBar() {
 	const { cartItems } = useContext(CartContext);
 	return (
 		<>
-				<nav id="navBar" className="flex">
+			<nav id="navBar" className="flex">
 				<ul className="flex flex-1 justify-evenly">
 					<li className="text-xl font-medium nav group flex justify-center items-center p-5">
 						<NavLink to="/" className="flex items-center gap-2">
 							<Home className="block" />
-							<span className="group-hover:underline underline-offset-10 decoration-white">Home</span>
+							<span className="group-hover:underline underline-offset-10 decoration-white">
+								Home
+							</span>
 						</NavLink>
 					</li>
 					<li className="text-xl font-medium nav group flex justify-center items-center p-5">
 						<NavLink to="/shop" className="flex items-center gap-2">
 							<Store className="block" />
-							<span className="group-hover:underline underline-offset-10 decoration-white">Shop</span>
+							<span className="group-hover:underline underline-offset-10 decoration-white">
+								Shop
+							</span>
 						</NavLink>
 					</li>
 					<li className="text-xl font-medium nav group flex justify-center items-center p-5">
 						<NavLink to="/cart" className="flex items-center gap-2">
 							<ShoppingCart className="block" />
-							<span className="group-hover:underline underline-offset-10 decoration-white">Cart</span>
-							<span id="counter" className="ml-3 border-3 rounded-sm h-8 w-6">
-								{cartItems.length > 0 && `${cartItems.length}`}
+							<span className="group-hover:underline underline-offset-10 decoration-white">
+								Cart
+							</span>
+							<span
+								id="counter"
+								className={`ml-3 border-2 rounded-sm h-8 w-6 ${cartItems.length === 0 && "opacity-0"}`}
+							>
+								{`${cartItems.length}`}
 							</span>
 						</NavLink>
 					</li>
