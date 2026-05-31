649f72145e868b06ed24550bfbb63a7b0c58beb8
rygyfygy <237001230+rygyfygy@users.noreply.github.com>
2026-05-31 20:42:48 +0200

    Add "add to cart" button on product details modal. Style for responsiveness.

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -4,6 +4,7 @@ import ProductCard from "../components/ProductCard";
 import "./Shop.css";
 import { CartContext } from "../context/CartContext";
 import useShopItems from "../hooks/useShopItems";
+import { Plus, X } from "lucide-react";
 
 function ProductDetails({
 	product,
@@ -15,39 +16,75 @@ function ProductDetails({
 	onAddToCart: () => void;
 }): React.ReactElement {
 	const close = () => setSelectedProduct(null);
+	const [added, setAdded] = useState(false);
 	return (
 		<div
-			className="fixed inset-0 z-50 flex align-center items-center justify-center bg-black/60"
+			className="fixed inset-0 z-50 flex items-center justify-center bg-black/60"
 			onClick={close}
 		>
 			<div
-				className="overflow-y-auto bg-(--details) text-(--text) rounded-xl p-8 max-w-lg w-full mx-4 flex flex-col items-center"
+				id="detailsModal"
+				className="m-1 p-3 bg-(--details) text-(--text) rounded-lg max-w-lg w-dvw h-dvh flex flex-col justify-start items-center gap-3 md:gap-2"
 				onClick={(e) => e.stopPropagation()}
 			>
 				<button
-					className="self-end text-gray-500 hover:text-black"
+					className="mr-3 self-end text-(--text-h) text-2xl hover:text-black"
 					onClick={close}
 				>
-					✕
+					<X />
 				</button>
-				<div className="flex justify-center items-center h-70 w-70 p-5 bg-white">
+				<div className="flex mb-2 justify-center items-center md:h-70 md:w-70 h-80 w-80 p-10 m-0 bg-white">
 					<img
 						src={product?.image}
-						className="h-55 drop-shadow-lg object-contain"
+						className="md:h-60 w-full drop-shadow-lg md:object-contain m-0"
 					/>
 				</div>
-				<h2 className="pt-5 text-(--text-h) text-lg font-bold">
-					{product?.title}
-				</h2>
-				<p className="text-sm text-gray-500">
-					{product?.rating?.rate}/5.00 ({product?.rating?.count}{" "}
-					reviews)
-				</p>
-				<p className="text-xl font-semibold">
-					${product?.price.toFixed(2)}
-				</p>
-				<p className="h-40 flex text-sm">{product?.description}</p>
-				<button onClick={onAddToCart}>ADD TO CART</button>
+				{/* ITEM DETAILS */}
+				<div
+					id="itemDetails"
+					className="flex-1 flex flex-col justify-start gap-1"
+				>
+					<h2
+						id="itemTitle"
+						className="text-center text-(--text-h) text-lg font-bold"
+					>
+						{product?.title}
+					</h2>
+					<div
+						id="itemRatingPrice"
+						className="flex items-center justify-between px-6 py-1"
+					>
+						<p className="align-text-center text-sm text-gray-500 m-0 p-0">
+							{product?.rating?.rate}/5.00 (
+							{product?.rating?.count} reviews)
+						</p>
+						<p className="align-text-center text-xl font-semibold">
+							${product?.price.toFixed(2)}
+						</p>
+					</div>
+				<div className="md:max-h-40 max-h-60 overflow-y-auto text-sm">
+					<p
+						className="text-justify text-pretty"
+						id="itemDescription"
+					>
+						{product?.description}
+					</p>
+				</div>
+				</div>
+				<button
+					className="md:my-1 md:py-1 my-2 py-2 w-60 justify-center shrink-0 flex flex-row items-center bg-black cursor-pointer"
+					onClick={() => {
+						onAddToCart();
+						setAdded(true);
+					}}
+				>
+					<span className="text-(--plus) px-5">
+						{added ? "ADDED TO CART" : "ADD TO CART"}
+					</span>
+					<div className="size-14 flex items-center justify-center shrink-0">
+						<Plus color="var(--plus)" />
+					</div>
+				</button>
 			</div>
 		</div>
 	);
@@ -88,7 +125,9 @@ export default function Shop() {
 				<ProductDetails
 					product={selectedProduct}
 					setSelectedProduct={setSelectedProduct}
-					onAddToCart={() => selectedProduct && addToCart(selectedProduct)}
+					onAddToCart={() =>
+						selectedProduct && addToCart(selectedProduct)
+					}
 				/>
 			)}
 
@@ -99,7 +138,7 @@ export default function Shop() {
 				{ITEM_CATEGORIES.map((category) => (
 					<div
 						key={category}
-						className={"category hover:text-gray-100"}
+						className={"category hover:text-(--accent-bg)"}
 					>
 						<button
 							className={`capitalize ${category == selectedCategory && "active"}`}
