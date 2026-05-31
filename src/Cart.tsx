50313a35ba1741a1c9a07b35deabc729e5e0353c
rygyfygy <237001230+rygyfygy@users.noreply.github.com>
2026-05-31 02:02:39 +0200

    Hide checkout button on empty cart

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -16,37 +16,39 @@ export default function Cart() {
 			{cartItems.length === 0 ? (
 				<p className="py-5 text-center">Your cart is empty!</p>
 			) : (
-				<table className="w-full border-collapse max-md:block">
-					<thead className="table-header-group max-md:hidden">
-						<tr className="text-sm text-(--text) border-b-2 border-(--border)">
-							<th />
-							<th />
-							<th>Product</th>
-							<th>Price</th>
-							<th>Qty.</th>
-							<th>Subtotal</th>
-						</tr>
-					</thead>
-					<tbody className="max-md:block max-md:space-y-3">
-						{cartItems.map((item) => (
-							<CartItem item={item} key={item.id} />
-						))}
-					</tbody>
-					<tfoot className="max-md:block">
-						<tr className="border-t-2 border-(--border) max-md:block">
-							<td
-								colSpan={6}
-								className="pt-5 px-5 text-right font-medium max-md:px-0"
-							>
-								Total:&nbsp; ${priceTotal.toFixed(2)}
-							</td>
-						</tr>
-					</tfoot>
-				</table>
+				<>
+					<table className="w-full border-collapse max-md:block">
+						<thead className="table-header-group max-md:hidden">
+							<tr className="text-sm text-(--text) border-b-2 border-(--border)">
+								<th />
+								<th />
+								<th>Product</th>
+								<th>Price</th>
+								<th>Qty.</th>
+								<th>Subtotal</th>
+							</tr>
+						</thead>
+						<tbody className="max-md:block max-md:space-y-3">
+							{cartItems.map((item) => (
+								<CartItem item={item} key={item.id} />
+							))}
+						</tbody>
+						<tfoot className="max-md:block">
+							<tr className="border-t-2 border-(--border) max-md:block">
+								<td
+									colSpan={6}
+									className="pt-5 px-5 text-right font-medium max-md:px-0"
+								>
+									Total:&nbsp; ${priceTotal.toFixed(2)}
+								</td>
+							</tr>
+						</tfoot>
+					</table>
+					<button className="bg-(--social-bg) hover:bg-(--accent-bg) text-(--text) border border-(--border) p-3 m-5">
+						Proceed to checkout
+					</button>
+				</>
 			)}
-			<button className="bg-(--social-bg) hover:bg-(--accent-bg) text-(--text) border border-(--border)  p-3 m-5">
-				Proceed to checkout
-			</button>
 		</div>
 	);
 }
